3a72ab5e6245917fce45f9a9bdc8938ef7564271
Juan Pablo Roldan <juancaicedo1997@gmail.com>
2026-03-06 18:35:52 -0500

    Añadido archivo de scripts con las consultas SQL.
    Modificada la conexión para añadir más rutas.
    
    Añadida una tabla para mostrar los datos de la base de datos en la vista home.php.

--- modelos/connection.php
+++ modelos/connection.php
@@ -10,7 +10,7 @@
             $this->host = "localhost";
             $this->user = "root";
             $this->pass = "root";
-            $this->db = "bd_usuario_test";
+            $this->db = "bd_usuarios_test";
 
             $this->con =mysqli_connect($this->host, $this->user, $this->pass, $this->db );
             if(mysqli_errno($this->con)){
--- vistas/home.php
+++ vistas/home.php
@@ -1,1 +1,57 @@
+<?php
+    $controlador = new Controlador();
+    $resultado = $controlador -> index();
+
+?>
+
 <h1> Modulo Home </h1>
+<head>
+    <meta charset="UTF-8">
+    <title>Lista de Usuarios</title>
+</head>
+<body>
+
+<h2>Listado de Usuarios</h2>
+
+<table border ="1">
+    <thead>
+        <tr> <!--CABECERA DE LA TABLA-->
+            <th>ID</th>
+            <th>Nombres</th>
+            <th>Apellidos</th>
+            <th>Cédula</th>
+            <th>Usuario</th>
+            <th>Password</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+    <tbody> <!--CUERPO DE LA TABLA-->
+        <tr> <!--USUARIO 1-->
+            <td>1</td>
+            <td>Juan</td>
+            <td>Pérez</td>
+            <td>12345678</td>
+            <td>jperez</td>
+            <td>123456789</td>
+            <td>
+                <a href="?cargar=consultar">Consultar</a>
+                <a href="?cargar=editar">Editar</a>
+                <a href="?cargar=eliminar">Eliminar</a>
+            </td>
+        </tr>
+        <tr> <!--USUARIO 2-->
+            <td>2</td>
+            <td>María</td>
+            <td>Gómez</td>
+            <td>87654321</td>
+            <td>mgomez</td>
+            <td>123456789</td>
+            <td>
+                <a href="?cargar=consultar">Consultar</a>
+                <a href="?cargar=editar">Editar</a>
+                <a href="?cargar=eliminar">Eliminar</a>
+            </td>
+        </tr>
+    </tbody>
+</table>
+</body>
